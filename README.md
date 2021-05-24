9d3607a1efb7dd99e8466a861a7bd9c3166ff367
liangyaowen.awen <liangyaowen.awen@bytedance.com>
2021-05-24 21:12:40 +0800

    chore(repository): 仓库迁移

---
+++ README.md
@@ -0,0 +1,13 @@
+# utils
+
+##
+文档: https://awen-hub.github.io/utils/#/
+## 项目全局依赖
+包管理工具: pnpm、yarn
+命令行工具: node、npx
+文档工具: jsdoc、jdsoc2md、conventional-changelog、docsify
+格式校验工具: eslint、prettier
+打包工具: rollup
+
+## 特殊命令
+加husky: npx husky add .husky/commit-msg 'npx --no-install commitlint --edit "$1"'
---
+++ release-rules.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    type: "feat",
+    release: "minor",
+  },
+  {
+    type: "fix",
+    release: "patch",
+  },
+  {
+    type: "perf",
+    release: "patch",
+  },
+  {
+    type: "revert",
+    release: "patch",
+  },
+  {
+    type: "refactor",
+    release: "minor",
+  },
+  {
+    type: "ci",
+    release: "patch",
+  },
+];
