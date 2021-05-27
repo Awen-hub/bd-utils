46b3d5852d3273dbd3554847dc528a1e40a07ce0
liangyaowen.awen <liangyaowen.awen@bytedance.com>
2021-05-27 10:55:40 +0800

    fix(global): 修复测试和文档

--- release-rules.js
+++ release-rules.js
@@ -3,24 +3,24 @@ module.exports = [
     type: "feat",
     release: "patch",
   },
-  {
-    type: "fix",
-    release: "patch",
-  },
-  {
-    type: "perf",
-    release: "patch",
-  },
-  {
-    type: "revert",
-    release: "patch",
-  },
-  {
-    type: "refactor",
-    release: "minor",
-  },
-  {
-    type: "ci",
-    release: "patch",
-  },
+  // {
+  //   type: "fix",
+  //   release: "patch",
+  // },
+  // {
+  //   type: "perf",
+  //   release: "patch",
+  // },
+  // {
+  //   type: "revert",
+  //   release: "patch",
+  // },
+  // {
+  //   type: "refactor",
+  //   release: "minor",
+  // },
+  // {
+  //   type: "ci",
+  //   release: "patch",
+  // },
 ];
